c11bb1c657e5c8e60562591e23d0fbfc333a2584
das-110722 <phuocvu.hcmc@gmail.com>
2024-06-25 02:55:23 +0700

    change_25-6

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+const images = {
+    logo: require('~/assets/images/logo.svg').default,
+    image: require('~/assets/images/Vi.jpg'),
+};
+
+export default images;
---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,23 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import images from '~/assets/images';
+
+const cx = classNames.bind(styles);
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img className={cx('avatar')} src={images.image} alt="Vi" />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Huyen Vi</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>@huyenvi</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
