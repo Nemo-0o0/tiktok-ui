68a406eb7299b9a6be3634c62c8bc1c44c4903ac
das-110722 <phuocvu.hcmc@gmail.com>
2024-06-27 18:06:40 +0700

    change2_27-6

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,22 +2,22 @@ import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
-import images from '~/assets/images';
+import { Link } from 'react-router-dom';
 import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <Image className={cx('avatar')} src={images.image} alt="Vi" />
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>Huyen Vi</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <span className={cx('username')}>@huyenvi</span>
+                <span className={cx('username')}>{data.nickname}</span>
             </div>
-        </div>
+        </Link>
     );
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
+// Layouts
 import { HeaderOnly } from '~/components/Layout';
+
+// Pages
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
-import Profile from '~/pages/Profie';
+import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
 
@@ -9,7 +12,7 @@ import Search from '~/pages/Search';
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
-    { path: '/profile/', component: Profile },
+    { path: '/:nickname', component: Profile },
     { path: '/upload', component: Upload, layout: HeaderOnly },
     { path: '/search', component: Search, layout: null },
 
